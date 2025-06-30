77bcd7140531712f6ab792c6aaf4c396c54e047a
Devin Chaney <shout64@gmail.com>
2025-06-29 22:34:18 -0400

    Add argument functionality

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -14,13 +15,21 @@ else:
     print("ERROR: No prompt provided")
     sys.exit(1)
 
+messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+]
+
 response = client.models.generate_content(
-    model="gemini-2.0-flash-001", contents=prompt
+    model="gemini-2.0-flash-001",
+    contents=messages
 )
 
 print(response.text)
-print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
-print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+if len(sys.argv) >= 3 and sys.argv[2] == "--verbose":
+    print(f"User prompt: {prompt}")
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
 
 
 # You’re certainly on the right track! By checking len(sys.argv) > 1, you’re making sure the user provides at least one argument (the prompt). Nicely reasoned.
