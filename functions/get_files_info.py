2ac0b140c63841ff53a72a2c93326ba56446a622
Devin Chaney <shout64@gmail.com>
2025-07-26 00:55:46 -0400

    Update logic for keeping work within working directory

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,24 +1,25 @@
 import os
 
 def get_files_info(working_directory, directory="."):
-    if directory != None:
-        full_path = os.path.join(working_directory, directory)
-    else:
-        full_path = working_directory
+    full_path    = os.path.join(working_directory, directory)
     
-    
-    work_dir_abs = os.path.abspath(directory)
+    work_dir_abs = os.path.abspath(working_directory)
+    dir_abs      = os.path.abspath(full_path)
 
-    if not work_dir_abs.startswith(working_directory):
+    if not dir_abs.startswith(work_dir_abs):
         return f'Error: Cannot list "{directory}" as it is outside the permitted working directory'
 
-    if not os.path.isdir(directory):
+    if not os.path.isdir(dir_abs):
         return f'Error: "{directory}" is not a directory'
 
-    dir_contents_list = os.listdir(full_path)
-    contents_string   = ""
+    dir_contents_list = os.listdir(dir_abs)
+    
+    if directory == ".":
+        contents_string = "Results for current directory:\n"
+    else:
+        contents_string = f"Results for '{directory}' directory:\n" 
     for item in dir_contents_list:
-        item_path = os.path.join(directory, item)
+        item_path = os.path.join(dir_abs, item)
         contents_string += f"- {item}: file_size={os.path.getsize(item_path)}, is_dir={os.path.isfile(item_path)}"
         if item != dir_contents_list[-1]:
             contents_string += "\n"
--- tests.py
+++ tests.py
@@ -1,3 +1,9 @@
 from functions.get_files_info import get_files_info
 
-print(get_files_info("calculator", "."))
+print(get_files_info("calculator", "."), "\n")
+
+print(get_files_info("calculator", "pkg"), "\n")
+
+print(get_files_info("calculator", "/bin"), "\n")
+
+print(get_files_info("calculator", "../"), "\n")
