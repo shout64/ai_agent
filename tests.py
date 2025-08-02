e8ae0518fbc71ccde66ca310c989134befcced50
Devin Chaney <shout64@gmail.com>
2025-08-02 18:08:28 -0400

    Finish get_files_info function

--- tests.py
+++ tests.py
@@ -1,6 +1,16 @@
 from functions.get_files_info import *
 
+print("Lorem test")
+print(get_file_content("calculator", "lorem.txt"))
+
+print("\nMain test")
 print(get_file_content("calculator", "main.py"))
+
+print("\nCalculator.py test")
 print(get_file_content("calculator", "pkg/calculator.py"))
+
+print("\nbin/cat test")
 print(get_file_content("calculator", "/bin/cat"))
+
+print("\nnot exist test")
 print(get_file_content("calculator", "pkg/does_not_exist.py"))
