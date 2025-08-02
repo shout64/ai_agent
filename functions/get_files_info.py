9fd63643bd550fdf7f9cbc41d52e73426bd91b32
shout64 <shout64@gmail.com>
2025-08-02 08:58:13 -0400

    Update get_files_info

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -26,6 +26,25 @@ def get_files_info(working_directory, directory="."):
 
     return contents_string
 
+def get_file_content(working_directory, file_path):
+    work_dir_abs  = os.path.abspath(working_directory)
+    file_path_abs = os.path.abspath(file_path)
+
+    if not file_path_abs.startswith(work_dir_abs):
+        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory' 
+
+    if not os.path.isfile(file_path):
+        return f'Error: File not found or is not a regular file: "{file_path}"'
+
+    MAX_CHARS = 10000
+
+    try:
+        with (file_path_abs, "r") as file:
+            file_string = file.read(MAX_CHARS)
+
+    except Exception as e:
+        print(f"Error: {e}")
+
 
     # os.path.abspath(): Get an absolute path from a relative path
     # os.path.join(): Join two paths together safely (handles slashes)
--- tests.py
+++ tests.py
@@ -1,9 +1,6 @@
-from functions.get_files_info import get_files_info
+from functions.get_files_info import *
 
-print(get_files_info("calculator", "."), "\n")
-
-print(get_files_info("calculator", "pkg"), "\n")
-
-print(get_files_info("calculator", "/bin"), "\n")
-
-print(get_files_info("calculator", "../"), "\n")
+print(get_file_content("calculator", "main.py"))
+print(get_file_content("calculator", "pkg/calculator.py"))
+print(get_file_content("calculator", "/bin/cat"))
+print(get_file_content("calculator", "pkg/does_not_exist.py"))
