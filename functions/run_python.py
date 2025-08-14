25b7e9382e571a157f51aebdf04f953556cbc877
shout64 <shout64@gmail.com>
2025-08-14 17:47:12 -0400

    Complete run_python function

--- functions/run_python.py
+++ functions/run_python.py
@@ -1,15 +1,36 @@
+import subprocess
+import os
+
 def run_python_file(working_directory, file_path, args=[]):
     full_path     = os.path.join(working_directory, file_path)
     work_dir_abs  = os.path.abspath(working_directory)
     file_path_abs = os.path.abspath(full_path)
 
     if not file_path_abs.startswith(work_dir_abs):
-        return f'Error: Cannot read "{file_path}" as it is outside the permitted working directory' 
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
 
     if not os.path.isfile(full_path):
-        return f'Error: File not found or is not a regular file: "{file_path}"'
+        return f'Error: File "{file_path}" not found.'
 
     if not full_path.endswith('.py'):
-        f'Error: "{file_path}" is not a Python file.' 
+        return f'Error: "{file_path}" is not a Python file.' 
 
-    
+    commands = ['python', file_path_abs]
+    if args:
+        commands.extend(args)
+
+    try:
+        result = subprocess.run(commands, capture_output=True, timeout=30, text=True, cwd=work_dir_abs)
+        
+        output = []
+        if result.stdout:
+            output.append(f"STDOUT: \n{result.stdout}")
+        if result.stderr:
+            output.append(f"STDERR: \n{result.stderr}")
+        if result.returncode != 0:
+            output.append(f"\nProccess exited with code {result.returncode}")
+
+        return "\n".join(output) if output else "No output produced."
+
+    except Exception as e:
+        return f"Error: executing Python file: {e}"
--- tests.py
+++ tests.py
@@ -1,5 +1,21 @@
-from functions.get_files_info import *
+from functions.run_python import *
 
-print(write_file("calculator", "lorem.txt", "wait, this isn't lorem ipsum"))
-print(write_file("calculator", "pkg/morelorem.txt", "lorem ipsum dolor sit amet"))
-print(write_file("calculator", "/tmp/temp.txt", "this should not be allowed"))
+print("First test")
+print(run_python_file("calculator", "main.py")) # (should print the calculator's usage instructions)
+print("=====================================")
+
+print("Second test")
+print(run_python_file("calculator", "main.py", ["3 + 5"])) # (should run the calculator... which gives a kinda nasty rendered result)
+print("=====================================")
+
+print("Third test")
+print(run_python_file("calculator", "tests.py"))
+print("=====================================")
+
+print("Fourth test")
+print(run_python_file("calculator", "../main.py")) # (this should return an error)
+print("=====================================")
+
+print("Fifth Test")
+print(run_python_file("calculator", "nonexistent.py")) # (this should return an error)
+print("=====================================")
