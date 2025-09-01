b0b00e6e8aad752cdeebf0e52cf21ef1abf1595a
Devin Chaney <shout64@gmail.com>
2025-09-01 11:34:07 -0400

    Add LLM function calls

--- functions/get_files_info.py
+++ functions/get_files_info.py
@@ -1,4 +1,5 @@
 import os
+from google.genai import types
 
 def get_files_info(working_directory, directory="."):
     full_path    = os.path.join(working_directory, directory)
@@ -25,5 +26,19 @@ def get_files_info(working_directory, directory="."):
             contents_string += "\n"
 
     return contents_string
-            file.write(content)
+        # file.write(content)
+
+schema_get_files_info = types.FunctionDeclaration(
+    name="get_files_info",
+    description="Lists files in the specified directory along with their sizes, constrained to the working directory.",
+    parameters=types.Schema(
+        type=types.Type.OBJECT,
+        properties={
+            "directory": types.Schema(
+                type=types.Type.STRING,
+                description="The directory to list files from, relative to the working directory. If not provided, lists files in the working directory itself.",
+            ),
+        },
+    ),
+)
 
